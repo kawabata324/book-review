6a995f53b7df6027ad9987a6d90bdc63cfa14359
kawabata <s1931115@s.konan-u.ac.jp>
2022-07-01 17:26:19 +0900

    add

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  assetsDir: './',
-  outDir: 'docs',
+  base: '/book-review/',
   plugins: [react()],
   server: {
     port: 8000,
